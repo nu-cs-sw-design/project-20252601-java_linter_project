8413e2a1ac17c1addc11211865524b947440d5ca
Dev Shah <shahdev212@gmail.com>
2025-12-06 23:42:27 -0600

    unused private method check

---
+++ src/main/java/refactored/domain/UnusedPrivateMethodCheck.java
@@ -0,0 +1,51 @@
+package refactored.domain;
+
+import refactored.datasource.internal_model.ClassData;
+import refactored.datasource.internal_model.MethodData;
+import refactored.datasource.internal_model.MethodReference;
+import refactored.presentation.LintResult;
+import refactored.presentation.Severity;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UnusedPrivateMethodCheck extends AbstractLintCheck {
+
+    public UnusedPrivateMethodCheck() {
+        super(CheckType.UNUSED_PRIVATE_METHOD,
+              "Unused Private Method",
+              "Checks for private methods that are never called");
+    }
+
+    @Override
+    public List<LintResult> check(ClassData classData) {
+        List<LintResult> results = new ArrayList<>();
+        List<MethodData> allMethods = classData.getMethods();
+
+        for (MethodData method : allMethods) {
+            if (method.isPrivate() && !method.isConstructor()) {
+                if (!isMethodCalled(method, allMethods)) {
+                    String message = String.format("Private method '%s' is never used", method.getName());
+                    String location = classData.getClassName() + "." + method.getName();
+                    results.add(createResult(Severity.WARNING, message, location));
+                }
+            }
+        }
+
+        return results;
+    }
+
+    private boolean isMethodCalled(MethodData targetMethod, List<MethodData> allMethods) {
+        for (MethodData method : allMethods) {
+            if (method == targetMethod) {
+                continue;
+            }
+
+            for (MethodReference calledMethod : method.getCalledMethods()) {
+                if (calledMethod.matches(targetMethod)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
